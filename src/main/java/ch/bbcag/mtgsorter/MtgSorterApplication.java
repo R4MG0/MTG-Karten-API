35d41fceafea53236e27e2cd6a7112e41975ff5c
Herren Marco <marco.herren@bbcag.ch>
2022-02-02 13:47:55 +0100

    Initial commit

---
+++ src/main/java/ch/bbcag/mtgsorter/MtgSorterApplication.java
@@ -0,0 +1,13 @@
+package ch.bbcag.mtgsorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgSorterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgSorterApplication.class, args);
+    }
+
+}
